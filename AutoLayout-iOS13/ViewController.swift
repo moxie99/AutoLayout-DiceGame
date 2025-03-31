c21792185e3801cc64dc02b060165598e6db0a20
Adeolu David <adeolusegun99@gmail.com>
2025-03-31 22:30:55 +0100

    Changed dice name from numerals to word

--- AutoLayout-iOS13/ViewController.swift
+++ AutoLayout-iOS13/ViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var diceImageView1: UIImageView!
-    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
         let allDice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
         
-        diceImageView1.image = allDice[Int.random(in: 0...5)]
-        diceImageView2.image = allDice[Int.random(in: 0...5)]
+        diceImageViewOne.image = allDice[Int.random(in: 0...5)]
+        diceImageViewTwo.image = allDice[Int.random(in: 0...5)]
         
     }
     
